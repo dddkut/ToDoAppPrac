8b3abe6ceb271289ee24d8d551135c5e8339d66a
KOBO <kutxxxzz@gmail.comwd>
2024-12-22 00:31:44 +0900

    adding taskController contents and the rest of taskService

--- backend/src/task/task.controller.ts
+++ backend/src/task/task.controller.ts
@@ -1,4 +1,51 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Param,
+  Body,
+  Req,
+} from '@nestjs/common';
+import { TaskService } from './task.service';
+import { CreateTaskDto, UpdateTaskDto } from 'src/dto/task.dto';
 
 @Controller('task')
-export class TaskController {}
+export class TaskController {
+  constructor(private readonly taskService: TaskService) {}
+
+  @Post()
+  createTask(@Req() req, @Body() createTaskDto: CreateTaskDto) {
+    const userId = req.user.uid; // Firebase Authentication uid
+    return this.taskService.createTask(userId, createTaskDto);
+  }
+
+  @Get()
+  getUserTasks(@Req() req) {
+    const userId = req.user.uid;
+    return this.taskService.getUserTasks(userId);
+  }
+
+  @Get(':id')
+  getTask(@Req() req, @Param('id') id: string) {
+    const userId = req.user.uid;
+    return this.taskService.getTask(userId, id);
+  }
+
+  @Patch(':id')
+  updateTask(
+    @Req() req,
+    @Param('id') id: string,
+    @Body() updateTaskDto: UpdateTaskDto,
+  ) {
+    const userId = req.user.uid;
+    return this.taskService.updateTask(userId, id, updateTaskDto);
+  }
+
+  @Delete(':id')
+  deleteTask(@Req() req, @Param('id') id: string) {
+    const userId = req.user.uid;
+    return this.taskService.deleteTask(userId, id);
+  }
+}
--- backend/src/task/task.service.ts
+++ backend/src/task/task.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from '@nestjs/common';
-import { CreateTaskDto } from 'src/dto/task.dto';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateTaskDto, UpdateTaskDto } from 'src/dto/task.dto';
 import { Task } from 'src/entity/task.entity';
 import { FirebaseService } from 'src/firebase/firebase.service';
 import { v4 as uuid } from 'uuid';
@@ -10,6 +10,33 @@ export class TaskService {
 
   private readonly db = this.firebaseService.getFirestore();
 
+  // get task list for a user
+  async getUserTasks(userId: string): Promise<Task[]> {
+    const snapshot = await this.db
+      .collection('tasks')
+      .where('userId', '==', userId)
+      .get();
+
+    if (snapshot.empty) {
+      return [];
+    }
+
+    return snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }) as Task);
+  }
+
+  // get a task
+  async getTask(userId: string, taskId: string): Promise<Task> {
+    const docRef = this.db.collection('tasks').doc(taskId);
+    const snapshot = await docRef.get();
+
+    if (!snapshot.exists || snapshot.data()?.userId !== userId) {
+      throw new NotFoundException('Task not found or access denied.');
+    }
+
+    return { id: snapshot.id, ...snapshot.data() } as Task;
+  }
+
+  //create a task
   async createTask(
     userId: string,
     createTaskDto: CreateTaskDto,
@@ -27,5 +54,42 @@ export class TaskService {
     const result = await docRef.get();
 
     return { id: result.id, ...result.data() } as Task;
+  }
+
+  //update a task
+  async updateTask(
+    userId: string,
+    taskId: string,
+    updateTaskDto: UpdateTaskDto,
+  ): Promise<Task> {
+    const docRef = this.db.collection('tasks').doc(taskId);
+    const snapshot = await docRef.get();
+
+    if (!snapshot.exists || snapshot.data()?.userId !== userId) {
+      throw new NotFoundException('Task not found or access denied.');
+    }
+
+    const updatedData = {
+      ...updateTaskDto,
+      updatedAt: new Date().getTime(),
+    };
+
+    await docRef.update(updatedData);
+    const updatedSnapshot = await docRef.get();
+
+    return { id: updatedSnapshot.id, ...updatedSnapshot.data() } as Task;
+  }
+
+  // タスクを削除
+  async deleteTask(userId: string, taskId: string): Promise<boolean> {
+    const docRef = this.db.collection('tasks').doc(taskId);
+    const snapshot = await docRef.get();
+
+    if (!snapshot.exists || snapshot.data()?.userId !== userId) {
+      throw new NotFoundException('Task not found or access denied.');
+    }
+
+    await docRef.delete();
+    return true;
   }
 }
