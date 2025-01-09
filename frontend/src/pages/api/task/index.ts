f218f2e0ba19fc9e36cbe54f90dd02c44bbe2de3
KOBO <kutxxxzz@gmail.comwd>
2025-01-09 23:20:25 +0900

    preparing for redux

--- frontend/src/pages/api/task/index.ts
+++ frontend/src/pages/api/task/index.ts
@@ -9,18 +9,19 @@ export default async function handler(
   res: NextApiResponse
 ) {
   // res.status(200).json({ name: "John Doe" });
+  const token = localStorage.getItem("token");
 
   console.log("bbbbbbbbbbbbbbbbb!!!!!!!!!!!!!!!!!!!!!!!!!!!");
   try {
     if (req.method === "GET") {
       //accessing to backend
-      const response = await axios.get(`${NEST_API_BASE_URL}/task`);
-      // const response = await axios.get(`${NEST_API_BASE_URL}/task`, {
-      //TODO:tokenを追加する
-      //   headers: {
-      //     Authorization: `Bearer ${token}`,
-      //   },
-      // });
+      // const response = await axios.get(`${NEST_API_BASE_URL}/task`);
+      const response = await axios.get(`${NEST_API_BASE_URL}/task`, {
+        //TODO:tokenを追加する
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
       return res.status(200).json(response.data);
     }
   } catch (error: any) {
